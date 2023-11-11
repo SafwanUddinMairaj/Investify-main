82a35c563ab489454020540da718aa6ffdb21b99
Safwan Uddin Mairaj <SafwanUddinMairaj>
2023-11-11 23:05:50 +0500

    added chat feature

---
+++ client/src/resources/Chat/Chatbox.js
@@ -0,0 +1,12 @@
+import { Stack } from '@chakra-ui/react'
+import React from 'react'
+import { Link } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom"; 
+
+const Chat = () => {
+
+    
+ 
+};
+
+export default Chat
